8d8fdf70ab08b38c6250d1ed9d043a7646cfcea2
IuryBRIGNOLI <iury.195018@alunosatc.edu.br>
2021-10-25 16:30:50 -0300

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,8 @@
+aaaa = 1
+menu = int(input("1-Somar \n2-Multiplicar \n3-Maior \n4-Menor \n5-Sair"))
+a = int(input("Digite um valor "))
+b =  int(input("Digite outro valor"))
+
+while(aaaa==1):
+    if menu == 1 :
+        print(a+b)
