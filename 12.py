0f3c32639939cd580796c9803aa1cfa2f1afa458
IuryBRIGNOLI <iury.195018@alunosatc.edu.br>
2021-10-25 16:37:15 -0300

    Update 12.py

--- 12.py
+++ 12.py
@@ -6,3 +6,22 @@ b =  int(input("Digite outro valor"))
 while(aaaa==1):
     if menu == 1 :
         print(a+b)
+        break
+    if menu == 2 :
+        print(a*b)
+        break
+    if menu == 3 and a > b:
+        print(a)
+        break
+    else:
+        print(b)
+        break
+    if menu == 4 and a < b:
+        print (b)
+        break
+    else:
+        print(a)
+        break
+    if menu ==5 :
+        aaaa = 0 
+        break                   
